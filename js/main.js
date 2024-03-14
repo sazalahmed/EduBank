53e1e11520c9dde93a5c135f651e4d5422c245aa
freelancersazal <freelancersazal@gmail.com>
2024-03-14 17:27:10 +0600

    added three new pages

--- js/main.js
+++ js/main.js
@@ -585,7 +585,12 @@ $(function () {
 
 
 
-
+    $(function () {
+        $('#calendar_js').calendar({
+            months: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+            days: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        });
+    });
 
 
 
